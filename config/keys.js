ad53493a195fe3c86d658ce3b73df62b1eb8d768
rukundo fabrice <rukundofabrice111@gmail.com>
2017-12-05 12:24:09 +0200

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb://fabryrukundo:rukundo111@ds139342.mlab.com:39342/storybooks-dev',
+    googleClientID:'319121550535-n30c1ofgomfkn1nb1a3ufcv8h7p6icid.apps.googleusercontent.com',
+    googleClientSecret:'R8NHbuWx4BxF-F_jNvAajgkR'
+
+}
