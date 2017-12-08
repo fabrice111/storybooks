ecda4aac7850f3e158dd291f1af4fc7de1a25ef5
rukundo fabrice <rukundofabrice111@gmail.com>
2017-12-08 19:29:28 +0200

    sss1

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
+module.exports = {
+  mongoURI:'mongodb://fabryrukundo:rukundo111@ds139342.mlab.com:39342/storybooks-dev',
+  googleClientID:'319121550535-tet5qa6ev9772n0jp324kep9du7eprfe.apps.googleusercontent.com',
+  googleClientSecret:'adR2ofNEAj4YfcIWja6IOsbu'
 }
