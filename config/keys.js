1e604f0e46025a4301952671b431d512f6ec914b
rukundo fabrice <rukundofabrice111@gmail.com>
2017-12-08 12:46:35 +0200

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb://localhost/storybooks',
+    mongoURI:'mongodb://fabryrukundo:rukundo111@ds139342.mlab.com:39342/storybooks-dev',
     googleClientID:'319121550535-tet5qa6ev9772n0jp324kep9du7eprfe.apps.googleusercontent.com',
     googleClientSecret:'adR2ofNEAj4YfcIWja6IOsbu'
   }
